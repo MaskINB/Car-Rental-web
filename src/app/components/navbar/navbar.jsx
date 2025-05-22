1b513a8bb4b205011c5fa0676da4ced649889cba
MaskINB <168406807+MaskINB@users.noreply.github.com>
2025-05-22 15:32:13 +0530

    crate a new buttion styles

--- src/app/components/navbar/navbar.jsx
+++ src/app/components/navbar/navbar.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
         </ul>
 
         {/* Auth Buttons (Right) */}
-        <div className="flex flex-col md:flex-row gap-3 font-bold mt-4 md:mt-0">
+        <div className="px-6 py-2 bg-gradient-to-r from-amber-400 to-yellow-300 text-black font-semibold rounded-lg shadow-md hover:shadow-lg hover:from-amber-500 hover:to-yellow-400 transition duration-300 ease-in-out">
           {navData.auth.map((item, index) => (
             <Link 
               key={index} 
