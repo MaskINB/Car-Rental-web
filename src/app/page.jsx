6c31f8fc5da1641e28b309362d9a016a6aba6509
MaskINB <168406807+MaskINB@users.noreply.github.com>
2025-07-12 21:53:09 +0530

    rename files

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import "./globals.css";
 import Carousel from "./components/carousel/carousel";
-import SecondSection from "./components/SecondSection/SecondSection";
+import SecondSection from "./components/FirstSection/firstsection";
 import Thirdsection from "./components/Thirdsection/thirdsection";
 import ForthSection from "./components/ForthSection/forthSection";
 import Fifthsection from "./components/FifthSection/fifthsection";
